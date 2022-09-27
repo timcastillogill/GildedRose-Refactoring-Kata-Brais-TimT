8fbe942f96501efdb7c7f57ea667841750151d94
timcastillogill <timgillviola@gmail.com>
2022-09-27 10:58:13 +0100

    GREEN: tests added for back stage pass

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -29,5 +29,30 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals(1,app.items[0].sellIn);
     }
+    @Test
+    void backstage_pass_increases_in_quality_and_decreases_in_sell_when_older_than_10_days(){
+        Item[] items = new Item[]{ new Item("Backstage passes to a TAFKAL80ETC concert", 25, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(24,app.items[0].sellIn);
+        assertEquals(2,app.items[0].quality);
+    }
+
+    @Test
+    void sulfuras_quality_and_sellin_does_not_change(){
+        Item[] items = new Item[]{ new Item("Sulfuras, Hand of Ragnaros", 25, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(25,app.items[0].sellIn);
+        assertEquals(10,app.items[0].quality);
+    }
+
+    @Test
+    void back_stage_pass_with_sellin_greater_than_10_increase_quality_by_1(){
+        Item[] items = new Item[]{ new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11,app.items[0].quality);
+    }
 
 }
