a3a4a012d3bc209d048fc9be492f73e637e1542e
timcastillogill <timgillviola@gmail.com>
2022-09-29 15:00:49 +0100

    start refactor

--- src/main/java/com/gildedrose/refactor/GildedRoseRefactored.java
+++ src/main/java/com/gildedrose/refactor/GildedRoseRefactored.java
@@ -13,26 +13,20 @@ public class GildedRoseRefactored{
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        decreaseQuality(i);
-                    }
+                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    decreaseQuality(i);
                 }
             } else {
                 if (items[i].quality < 50) {
                     upgradeQuality(i);
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                upgradeQuality(i);
-                            }
+                        if (items[i].sellIn < 11 && items[i].quality < 50) {
+                            upgradeQuality(i);
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                upgradeQuality(i);
-                            }
+                        if (items[i].sellIn < 6 && items[i].quality < 50) {
+                            upgradeQuality(i);
                         }
                     }
                 }
@@ -45,13 +39,12 @@ public class GildedRoseRefactored{
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                decreaseQuality(i);
-                            }
+                        if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            decreaseQuality(i);
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        decreaseQuality(i);
+                        items[i].quality = 0;
                     }
                 } else {
                     if (items[i].quality < 50) {
